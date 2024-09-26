2a74bcb9894edac81a847a52ce3228d49e607f3b
lukastan1999 <lukastan1999@gmail.com>
2024-09-26 12:58:00 +0200

    izmene stranice

--- sok_project/myapp/views.py
+++ sok_project/myapp/views.py
@@ -1,3 +1,10 @@
+from django.apps import apps
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    config = apps.get_app_config('core').config
+    for k, v in config.items():
+        print(k, v)
+    return render(request, "index.html", {"title": "Index",
+                                          "config": config})
